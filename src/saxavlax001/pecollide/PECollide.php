f57de99dceee1b4102a6e5c15b8df6f508fa8ce5
Saxavlax001 <saxavlax001@gmail.com>
2019-08-07 23:28:38 +0300

    Make a change.

--- src/saxavlax001/pecollide/PECollide.php
+++ src/saxavlax001/pecollide/PECollide.php
@@ -11,26 +11,29 @@ class PECollide implements Listener
     {
 
         $player = $ev->getPlayer();
-
+      
         foreach ($player->getViewers() as $viewer)
         {
             //If player too far, continue
             if ($player->distance($viewer) > 0.5) continue;
             //Get speed of that close player
             $speed = abs($player->getMotion()->x) + abs($player->getMotion()->z);
-            //If player
+            //If player speed is superior to X
             if ($speed > 2)
             {
+				//Knock him,if he's running
+                $viewer->knockBack($player, 0, $viewer->x - $player->x, $viewer->z - $player->z, 0.3);
+                $player->knockBack($viewer, 0, $player->x - $viewer->x, $player->z - $viewer->z, 0.1);
+                break;
+            }
+            //If player speed is inferior to X
+            if ($speed < 2)
+            {
+				//Knock him,if he's running
                 $viewer->knockBack($player, 0, $viewer->x - $player->x, $viewer->z - $player->z, 0.2);
                 $player->knockBack($viewer, 0, $player->x - $viewer->x, $player->z - $viewer->z, 0.1);
-            }
-            if (abs($player->getMotion()
-                ->x) + abs($player->getMotion()
-                ->z) < 2)
-            {
-                $ev->setCancelled();
+                break;
             }
         }
     }
-    
-}
+  }
