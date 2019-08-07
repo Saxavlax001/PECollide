2c7f710b96c10faec73cd97ca9aba1bb30bc39bb
Benda95280 <1539408+benda95280@users.noreply.github.com>
2019-08-07 21:20:34 +0200

    Update PECollide.php

--- src/saxavlax001/pecollide/PECollide.php
+++ src/saxavlax001/pecollide/PECollide.php
@@ -1,24 +1,38 @@
 <?php
-
 namespace saxavlax001\pecollide;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 
-class PECollide implements Listener {
+class PECollide implements Listener
+{
 
-    public function onMove(PlayerMoveEvent $ev){
+    public function onMove(PlayerMoveEvent $ev)
+    {
 
-      $player = $ev->getPlayer();
+        $player = $ev->getPlayer();
 
-    foreach($player->getViewers() as $viewer) {
-        if($player->distance($viewer) > 0.5) continue;
-        if(abs($player->getMotion()->x) + abs($player->getMotion()->z) > 2);
-            $viewer->knockBack($player, 0, $viewer->x - $player->x, $viewer->z - $player->z, 0.2);
-            $player->knockBack($viewer, 0, $player->x - $viewer->x, $player->z - $viewer->z, 0.1);
-        if(abs($player->getMotion()->x) + abs($player->getMotion()->z) < 2);
-        $ev->setCancelled();
-            break;
-         }
-       }
+        foreach ($player->getViewers() as $viewer)
+        {
+            //If player too far, continue
+            if ($player->distance($viewer) > 0.5) continue;
+            //Get speed of that close player
+            $speed = abs($player->getMotion()
+                ->x) + abs($player
+                ->getMotion
+                ->z);
+            //If player
+            if ($speed > 2)
+            {
+                $viewer->knockBack($player, 0, $viewer->x - $player->x, $viewer->z - $player->z, 0.2);
+                $player->knockBack($viewer, 0, $player->x - $viewer->x, $player->z - $viewer->z, 0.1);
+            }
+            if (abs($player->getMotion()
+                ->x) + abs($player->getMotion()
+                ->z) < 2)
+            {
+                $ev->setCancelled();
+            }
+        }
     }
+    
