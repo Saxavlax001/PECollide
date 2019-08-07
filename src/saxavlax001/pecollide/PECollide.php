656bc956ca098172a45984967c30a23660a35038
Benda95280 <1539408+benda95280@users.noreply.github.com>
2019-08-07 22:45:09 +0200

    Update PECollide.php

--- src/saxavlax001/pecollide/PECollide.php
+++ src/saxavlax001/pecollide/PECollide.php
@@ -9,41 +9,39 @@ class PECollide implements Listener
 
     public function onMove(PlayerMoveEvent $ev)
     {
-
+		
         $player = $ev->getPlayer();
-      
+		//Speed calc
+		$from = $ev->getFrom();
+		$to = $ev->getTo();
+		echo "MyPos: $from - To: $to --- ";
+		$speed = round($from->distance($to),3);
+
         foreach ($player->getViewers() as $viewer)
         {
             //If player too far, continue
             if ($player->distance($viewer) > 0.5) continue;
             //Get speed of that close player
-            $speed = abs($player->getMotion()->x) + abs($player->getMotion->z);
-            //If player speed is superior to X
-            if ($speed > 2)
+            $speed = abs($player->getMotion()->x) + abs($player->getMotion()->z);
+            //If player speed is superior to 0.1, they knockBack
+            if ($speed > 0.1)
             {
 				//Knock Him, you'r running too fast
-                $viewer->knockBack($player, 0, $viewer->x - $player->x, $viewer->z - $player->z, 0.2);
-                $player->knockBack($viewer, 0, $player->x - $viewer->x, $player->z - $viewer->z, 0.1);
+				$knockvalue = $speed / 1.4;
+                $viewer->knockBack($player, 0, $viewer->x - $player->x, $viewer->z - $player->z, $knockvalue);
+                $player->knockBack($viewer, 0, $player->x - $viewer->x, $player->z - $viewer->z, $knockvalue);
             }
-            else
-            {
+			
+			//ToDo: Find if i'm going to hurt a player ? How ?
+			// Use the $to to know where i go
+			// Find the Viewer POS : $viewer->getX() ...
+			// Find a way to calculate if going inside the player (The area defined arround the user ?)
+			
+          //  else
+          //  {
 				//Hey, don't move !
-                $ev->setCancelled();
-            }
+          //      $ev->setCancelled();
+          //  }
         }
     }
-<<<<<<< HEAD
 }
-=======
-    
-}
-
-    foreach($player->getViewers() as $viewer) {
-        if($player->distance($viewer) > 0.5) continue;
-            $viewer->knockBack($player, 0, $viewer->x - $player->x, $viewer->z - $player->z, 0.2);
-            $player->knockBack($viewer, 0, $player->x - $viewer->x, $player->z - $viewer->z, 0.1);
-            break;
-         }
-       }
-    }
->>>>>>> e2385bc386d8245006d54ad4ed660b28acce5f5b
