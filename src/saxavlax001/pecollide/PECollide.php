82aab5cc68111677ad4ab9cd7d17a12efb1963ae
Saxavlax001 <saxavlax001@gmail.com>
2020-04-01 21:27:41 +0300

    Fix maybe?

--- src/saxavlax001/pecollide/PECollide.php
+++ src/saxavlax001/pecollide/PECollide.php
@@ -5,14 +5,6 @@ use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\Server;
 class PECollide implements Listener {
 
-
-    /** @var Main */
-    private $pg;
-
-    public function __construct()
-    {
-
-    }
     public function onMove(PlayerMoveEvent $ev) {
         $player = $ev->getPlayer();
         foreach ($player->getViewers() as $viewer) {
