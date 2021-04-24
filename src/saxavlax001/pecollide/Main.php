0f5332a7b5a35b6c2d6f5329449ac38d13861010
Saxavlax001 <saxavlax001@gmail.com>
2021-04-24 18:14:40 +0300

    fix again

--- src/saxavlax001/pecollide/Main.php
+++ src/saxavlax001/pecollide/Main.php
@@ -14,13 +14,13 @@ class Main extends PluginBase {
     public function onEnable() : void{
         self::$instance = $this;
         //register pecollide events
-        $this->getServer()->getPluginManager()->registerEvents(new PECollide($this), $this);
+        $this->getServer()->getPluginManager()->registerEvents(new PECollide(), $this);
         //register default config settings of plugin
 	    $this->dir = $this->getDataFolder();
 	    if(!is_dir($this->dir)) @mkdir($this->dir);
 	    if(!is_file($this->dir."config.yml")) $this->saveResource("config.yml");
         //register pecollide command
-        $this->getServer()->getCommandMap()->register("pecollide", new PECollide($this));
+        $this->getServer()->getCommandMap()->register("pecollide", new PECollide());
    }
 
    public static function getInstance() : Main {
